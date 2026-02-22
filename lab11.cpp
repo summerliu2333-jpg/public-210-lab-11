0d7889abf65925aae7a4a182764d9b67f17bd882
Yang Liu <summerliu2333@gmail.com>
2026-02-21 17:30:18 -0800

    Function prototypes and structure

--- lab11.cpp
+++ lab11.cpp
@@ -2,6 +2,24 @@
 #include <iostream>
 using namespace std;
 
+const int NR_BATCHES = 2, NR_PRODUCTS = 3;
+
+struct Product {
+    string name;
+    string sku;
+    int *batch_quantities;
+
+    //memory cleanup
+    ~Product() {
+        if (batch_quantities)
+            delete[] batch_quantities;
+        batch_quantities = nullptr;
+    }
+};
+
+void inputProduct(Product *);
+void displayProduct(Product *);
+
 int main() {
     return 0;
 }
